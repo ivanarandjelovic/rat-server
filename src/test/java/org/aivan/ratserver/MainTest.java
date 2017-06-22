69599941a853c501be2f15da21e9b8690102852f
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2017-06-22 19:13:38 +0200

    Add junit tests

---
+++ src/test/java/org/aivan/ratserver/MainTest.java
@@ -0,0 +1,16 @@
+package org.aivan.ratserver;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by aivan on 22/06/17.
+ */
+public class MainTest {
+
+    @Test
+    public void dummyTest() {
+        Assert.assertEquals(1,1);
+    }
+}
